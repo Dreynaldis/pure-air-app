2721aa3ec7c5e37b36b93b53b79af7e6ef7ce605
Dreynaldis <dion.reynaldis@gmail.com>
2024-11-17 22:21:33 +0700

    fixed middleware

--- server/src/middleware/middleware.ts
+++ server/src/middleware/middleware.ts
@@ -5,9 +5,7 @@ import {LEVEL, EXCEPTION_MESSAGE} from '../exceptions/EXCEPTION_MESSAGE';
 import logger from '../helpers/logger';
 import {v4 as uuidv4} from 'uuid';
 import {CustomException} from '../exceptions/CustomException';
-import {getPartner} from '../features/partner/partner.repository';
 import {base64Decode} from '../helpers/tools';
-import {PartnerAttributes} from '../db/models/Partner';
 
 export async function serviceMiddleware(
   req: Request,
@@ -33,43 +31,43 @@ async function checkAuthentication(
   res: Response,
   next: NextFunction,
 ) {
-  try {
-    const apiKey = req.headers['x-api-key'];
-    if (apiKey !== undefined && apiKey !== '') {
-      const decodedKey = base64Decode(apiKey.toString());
-      const decodedKeyObject = decodedKey.split(':');
-      const verifyKey: PartnerAttributes = await getPartner({
-        clientId: decodedKeyObject[0],
-        clientSecret: decodedKeyObject[1],
-      });
-      if (!verifyKey) {
-        throw new CustomException(EXCEPTION_MESSAGE.NOT_AUTHORIZED, {
-          message: 'Invalid api key',
-        });
-      }
-      // req.body.partnerId = verifyKey.id;
-      next();
-    } else {
-      throw new CustomException(EXCEPTION_MESSAGE.NOT_AUTHORIZED, {
-        message: 'Invalid api key',
-      });
-    }
-  } catch (e) {
-    if (e instanceof CustomException) {
-      if (e.obj.code >= 500) {
-        logger.log(LEVEL.ERROR, '[ERROR]', {
-          traceId: req.body.traceId,
-          statusCode: null,
-          message: e,
-        });
-      }
-    } else {
-      logger.log(LEVEL.ERROR, '[ERROR]', {
-        traceId: req.body.traceId,
-        statusCode: null,
-        message: e,
-      });
-    }
-    responseJSON(req, res, e, true);
-  }
+  // try {
+  //   const apiKey = req.headers['x-api-key'];
+  //   if (apiKey !== undefined && apiKey !== '') {
+  //     const decodedKey = base64Decode(apiKey.toString());
+  //     const decodedKeyObject = decodedKey.split(':');
+  //     const verifyKey: PartnerAttributes = await getPartner({
+  //       clientId: decodedKeyObject[0],
+  //       clientSecret: decodedKeyObject[1],
+  //     });
+  //     if (!verifyKey) {
+  //       throw new CustomException(EXCEPTION_MESSAGE.NOT_AUTHORIZED, {
+  //         message: 'Invalid api key',
+  //       });
+  //     }
+  //     // req.body.partnerId = verifyKey.id;
+  //     next();
+  //   } else {
+  //     throw new CustomException(EXCEPTION_MESSAGE.NOT_AUTHORIZED, {
+  //       message: 'Invalid api key',
+  //     });
+  //   }
+  // } catch (e) {
+  //   if (e instanceof CustomException) {
+  //     if (e.obj.code >= 500) {
+  //       logger.log(LEVEL.ERROR, '[ERROR]', {
+  //         traceId: req.body.traceId,
+  //         statusCode: null,
+  //         message: e,
+  //       });
+  //     }
+  //   } else {
+  //     logger.log(LEVEL.ERROR, '[ERROR]', {
+  //       traceId: req.body.traceId,
+  //       statusCode: null,
+  //       message: e,
+  //     });
+  //   }
+  //   responseJSON(req, res, e, true);
+  // }
 }
